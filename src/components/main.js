935580cf8e661714decedcdcf5c3f254c2cda3a3
jschaefer <jschaefer@3pc.de>
2020-12-28 11:55:57 +0100

    fix(main): set theme color for text and button

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import ThemeContext from '../styles/themecontext';
 
 const MainContainer = styled.main`
   display: flex;
@@ -15,12 +16,18 @@ const SVG = styled.svg`
   margin-left: 20px;
 `;
 
+const Button = styled.button`
+  color: ${(props) => props.color.foreground};
+`;
+
 const Main = () => {
+  const { color } = useContext(ThemeContext);
+
   return (
     <MainContainer className="container">
       <H1>
         Hello. I am Jessica a frontend developer from Berlin.
-        <button>
+        <Button color={color}>
           <SVG
             width="94"
             height="30"
@@ -31,11 +38,11 @@ const Main = () => {
           >
             <path
               d="M93.4142 16.4142C94.1953 15.6332 94.1953 14.3668 93.4142 13.5858L80.6863 0.857864C79.9052 0.0768158 78.6389 0.0768158 77.8579 0.857864C77.0768 1.63891 77.0768 2.90524 77.8579 3.68629L89.1716 15L77.8579 26.3137C77.0768 27.0948 77.0768 28.3611 77.8579 29.1421C78.6389 29.9232 79.9052 29.9232 80.6863 29.1421L93.4142 16.4142ZM0 17H92V13H0V17Z"
-              fill="black"
+              fill={color.foreground}
             />
           </SVG>
           read more
-        </button>
+        </Button>
       </H1>
     </MainContainer>
   );
