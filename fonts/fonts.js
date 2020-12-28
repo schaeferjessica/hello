eb0eda36506df5a220cd9f941126fcf2a62f760d
Jessica Schäfer <jschaefer@3pc.de>
2020-12-28 10:36:05 +0100

    Updates

---
+++ fonts/fonts.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components';
+import WorkSansFont300Woff from './work-sans-v8-latin-300.woff';
+import WorkSansFont300Woff2 from './work-sans-v8-latin-300.woff2';
+import WorkSansFont400Woff from './work-sans-v8-latin-regular.woff';
+import WorkSansFont400Woff2 from './work-sans-v8-latin-regular.woff2';
+import WorkSansFont500Woff from './work-sans-v8-latin-500.woff';
+import WorkSansFont500Woff2 from './work-sans-v8-latin-500.woff2';
+
+// TODO: to prevent flicker on pageLoad, use a css file instead (see https://dev.to/philschonholzer/don-t-use-font-face-in-styled-components-with-gatsbyjs-1hkg)
+export default createGlobalStyle`
+    /* work-sans-300 - latin */
+    @font-face {
+      font-family: 'Work Sans';
+      font-style: normal;
+      font-weight: 300;
+      font-display: swap;
+      src: url('${WorkSansFont300Woff}') format('woff2'), /* Super Modern Browsers */
+          url('${WorkSansFont300Woff2}') format('woff'); /* Modern Browsers */
+    }
+
+    /* work-sans-regular - latin */
+    @font-face {
+      font-family: 'Work Sans';
+      font-style: normal;
+      font-weight: 400;
+      font-display: swap;
+      src: url('${WorkSansFont400Woff}') format('woff2'), /* Super Modern Browsers */
+          url('${WorkSansFont400Woff2}') format('woff'); /* Modern Browsers */
+    }
+
+    /* work-sans-500 - latin */
+    @font-face {
+      font-family: 'Work Sans';
+      font-style: normal;
+      font-weight: 500;
+      font-display: swap;
+      src: url('${WorkSansFont500Woff}') format('woff2'), /* Super Modern Browsers */
+          url('${WorkSansFont500Woff2}') format('woff'); /* Modern Browsers */
+    }
+
+    html {
+      font-weight: 400;
+      font-size: 20px;
+      font-family: 'Work Sans';
+      line-height: 1.5;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      color: ${props => props.color.foreground};
+      background-color: ${props => props.color.background};
+    }
+`;
--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-04c8727760780093695b.js"
+    "url": "webpack-runtime-c3c4627010e4c3cc1688.js"
   },
   {
     "url": "framework-3e6dec6139dadaa7d0c7.js"
   },
   {
-    "url": "app-e75b1caf4112fcaf4429.js"
+    "url": "app-83e8d14df136b72fe433.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f520f7e25eaff857ddacb5ca5b57e24d"
+    "revision": "0c6915010a72c4c4837dd12c4d0b13f8"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "d918e1220338be4fdb0c2d0049f90602"
+    "revision": "7f938a08a4f8051a6fc5e13fe65361a0"
   },
   {
     "url": "polyfill-058a0cee7fa5777b59c0.js"
@@ -143,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/hello/app-e75b1caf4112fcaf4429.js`))) {
+  if (!resources || !(await caches.match(`/hello/app-83e8d14df136b72fe433.js`))) {
     return await fetch(event.request)
   }
 
