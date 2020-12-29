981d494a12c520bf52e44ab930d422abd374a003
jschaefer <jschaefer@3pc.de>
2020-12-29 13:07:43 +0100

    feat(main): adding main text animation

--- src/components/main.js
+++ src/components/main.js
@@ -1,88 +1,91 @@
-import React, { useContext, useState } from 'react';
+import React, { useRef, useEffect, useContext } from 'react';
 import styled from 'styled-components';
-import ThemeContext from '../styles/themecontext';
 import { devices } from '../styles/breakpoints';
+import ThemeContext from '../styles/themecontext';
+import anime from 'animejs/lib/anime.es.js';
 
 const MainContainer = styled.main`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
 `;
 const H1 = styled.h1`
-  font-weight: 400;
+  font-weight: 300;
   font-size: 65px;
+  line-height: 90px;
 
   @media ${devices.mobile} {
     font-size: 35px;
+    line-height: 60px;
   }
-`;
-const Text = styled.p`
-  font-weight: 400;
-  font-size: 65px;
-  display: block;
 
-  @media ${devices.mobile} {
-    font-size: 35px;
+  b {
+    font-weight: 500;
   }
-`;
-const SVG = styled.svg`
-  margin-right: 20px;
-  margin-left: 20px;
 
-  @media ${devices.mobile} {
-    width: 50px;
-    height: 18px;
-    margin-right: 10px;
-    margin-left: 10px;
+  .hello {
+    transform-origin: 50% 100%;
+    display: inline-block;
+    transform: scale(0);
   }
-`;
 
-const Button = styled.button`
-  color: ${(props) => props.color.foreground};
-  font-weight: 400;
-  font-size: 65px;
+  span {
+    display: inline-block;
+    opacity: 0;
+  }
 
-  @media ${devices.mobile} {
-    font-size: 35px;
+  a {
+    color: ${(props) => props.color};
   }
 `;
 
 const Main = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const hallo = useRef(null);
+  const intro = useRef(null);
   const { color } = useContext(ThemeContext);
+
+  useEffect(() => {
+    anime.timeline().add({
+      targets: hallo.current,
+      scale: [0, 1],
+      duration: 1500,
+      elasticity: 600,
+    });
+
+    anime.timeline().add({
+      targets: intro.current,
+      translateY: [100, 0],
+      translateZ: 0,
+      opacity: [0, 1],
+      easing: 'easeOutExpo',
+      duration: 1400,
+      delay: 1000,
+    });
+  }, []);
 
   return (
     <MainContainer className="container">
-      <H1>
-        Hello. I am Jessica a frontend developer from Berlin.
-        <Button color={color} onClick={() => setIsOpen(!isOpen)}>
-          <SVG
-            width="94"
-            height="30"
-            viewBox="0 0 94 30"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-          >
-            <path
-              d="M93.4142 16.4142C94.1953 15.6332 94.1953 14.3668 93.4142 13.5858L80.6863 0.857864C79.9052 0.0768158 78.6389 0.0768158 77.8579 0.857864C77.0768 1.63891 77.0768 2.90524 77.8579 3.68629L89.1716 15L77.8579 26.3137C77.0768 27.0948 77.0768 28.3611 77.8579 29.1421C78.6389 29.9232 79.9052 29.9232 80.6863 29.1421L93.4142 16.4142ZM0 17H92V13H0V17Z"
-              fill={color.foreground}
-            />
-          </SVG>
-          read {isOpen ? 'less' : 'more'}
-        </Button>
+      <H1 color={color.foreground}>
+        <b ref={hallo} className="hello">
+          Hello.
+        </b>{' '}
+        <span ref={intro}>
+          I am <b>Jessica</b> a <b>frontend developer</b> at{' '}
+          <b>
+            <a
+              href="https://3pc.de/agentur/team/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              3pc
+            </a>
+          </b>{' '}
+          from <b>Berlin</b>.
+        </span>
       </H1>
-
-      <Text
-        style={{
-          display: isOpen ? 'block' : 'none',
-        }}
-      >
-        I have always been a creative person and a technology enthusiast. I was
-        lucky to start my career at 3pc, where I got passionate about frontend
-        development.
-      </Text>
     </MainContainer>
   );
 };
