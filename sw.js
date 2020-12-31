0bd5836ff02f449ca3ef973ade641b333fb381fc
Jessica Schäfer <jschaefer@3pc.de>
2020-12-31 14:55:54 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-068dbeb4fd555d57d726.js"
+    "url": "webpack-runtime-e8aa50204adc3f3f45ec.js"
   },
   {
     "url": "framework-3e6dec6139dadaa7d0c7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f7f63abc682e26eb495f6fada5b77fd6"
+    "revision": "09496fe53d617bcce86b17de9b381b5c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-19245c8506e49b502b12.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "34648b8b3241a60259189a34ad76d9ee"
+    "revision": "4f99d9f493ff6ed2db06ba8252e12874"
   },
   {
     "url": "polyfill-058a0cee7fa5777b59c0.js"
