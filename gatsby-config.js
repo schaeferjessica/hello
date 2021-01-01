b24f5266f2907f5cad11ef5c6b11c0eb0846643e
jschaefer <jschaefer@3pc.de>
2021-01-01 17:33:56 +0100

    feat: adding siteMetaData and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,24 @@ module.exports = {
   pathPrefix: `/hello`,
   siteMetadata: {
     title: 'jessica schaefer',
+    siteUrl: `https://schaeferjessica.github.io/hello/`,
+    description: `Jessica Schäfer, frontend developer from berlin`,
   },
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `jessica-schaefer`,
+        short_name: `jessica`,
+        start_url: `/`,
+        background_color: `#FFF6E2`,
+        theme_color: `#10287F`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.svg`, // This path is relative to the root of the site.
+      },
+    },
   ],
 };
